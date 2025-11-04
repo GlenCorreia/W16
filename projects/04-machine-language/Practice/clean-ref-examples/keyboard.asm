17f5a3869d5be11bd5d813920f8263f64a5dd8b4
Glen Correia <glen_correia@outlook.com>
2025-11-04 10:12:54 +0530

    Implement Keyboard

---
+++ projects/04-machine-language/Practice/clean-ref-examples/keyboard.asm
@@ -0,0 +1,9 @@
+// To check which key is currently pressed:
+// Read the contents of RAM[24576] (address KBD)
+// If the register contains 0, no key is pressed
+
+@KBD
+D=M
+
+
+// Program is incomplete, needs to be furnished (worked upon further)

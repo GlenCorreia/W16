f8eb16ea3a12e4e57eeee2ea783bc9aa3a567905
Glen Correia <glen_correia@outlook.com>
2025-11-04 10:52:14 +0530

    Complete Mult.asm

--- projects/04-machine-language/mult/Mult.asm
+++ projects/04-machine-language/mult/Mult.asm
@@ -8,3 +8,51 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+  // Set number1 to R0
+  @0
+  D=M
+  @number
+  M=D
+
+  // Set i (number2) to R1
+  @1
+  D=M
+  @i
+  M=D
+
+  // Set sum to 0
+  @0
+  D=A
+  @sum
+  M=D
+
+(LOOP)
+
+  // if @i <= 0 goto END
+  @i
+  D=M
+  @END
+  D;JLE
+
+  @number
+  D=M
+  @sum
+  M=M+D
+
+  // increment the counter
+  @i
+  M=M-1
+  @LOOP
+  0;JMP
+(END)
+
+(END)
+  // assign @sum to RAM[2]
+  @sum
+  D=M
+  @2
+  M=D
+
+  @END
+  0;JMP
